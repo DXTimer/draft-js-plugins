47461e417b93677f3fea0ecd6fff476ed085efc0
DXTimer <ibbakalov@gmail.com>
2019-12-30 19:15:39 +0000

    prettier lint

--- docs/client/components/shared/Link/index.js
+++ docs/client/components/shared/Link/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import styles from './styles.css';
 
 export default class Link extends Component {
-
   render() {
     const { className, ...props } = this.props; // eslint-disable-line no-use-before-define
     const combinedClassName = clsx(styles.root, className);
